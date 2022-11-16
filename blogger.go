4e8f486b838e29ae6f73e5cfc60eaa4967b9e250
turnerem <turnerem@tcd.ie>
2022-11-16 16:10:17 +0000

    add Title to Post struct and extract this info in NewPostsFromFS

--- blogger.go
+++ blogger.go
@@ -3,16 +3,46 @@ package blogger
 
 import (
   "io/fs"
+  "io"
+
 )
 
 type Post struct {
+  Title string
 }
 
-func NewPostsFromFS(filesystem fs.FS) []Post {
-  dir, _ := fs.ReadDir(filesystem, ".")
-  var posts []Post
-  for range dir {
-    posts = append(posts, Post{})
+func NewPostsFromFS(filesystem fs.FS) ([]Post, error) {
+  dir, err := fs.ReadDir(filesystem, ".")
+
+  if err != nil {
+    return nil, err
   }
-  return posts
+
+  var posts []Post
+  for _, f := range dir {
+    post, err := getPost(filesystem, f)
+    if err != nil {
+      return nil, err
+    }
+
+    posts = append(posts, post)
+  }
+
+  return posts, nil
+}
+
+func getPost(filesystem fs.FS, f fs.DirEntry) (Post, error) {
+  postFile, err := filesystem.Open(f.Name())
+  if err != nil {
+    return Post{}, err
+  }
+  defer postFile.Close()
+
+  postData, err := io.ReadAll(postFile)
+  if err != nil {
+    return Post{}, err
+  }
+
+  post := Post{Title: string(postData)[7:]}
+  return post, nil
 }
--- blogger_test.go
+++ blogger_test.go
@@ -4,17 +4,49 @@ import (
   blogger "github.com/turnerem/blogger"
   "testing"
   "testing/fstest"
+  "io/fs"
+  "errors"
+  "reflect"
 )
 
 func TestNewBlogPosts(t *testing.T) {
-  fs := fstest.MapFS{
-    "hellowrld.md":	{Data: []byte("hi")},
-    "hey_worldie.md":	{Data: []byte("hiya")},
-  }
+  t.Run("returns all blog posts", func(t *testing.T) {
+    fs := fstest.MapFS{
+      "hellowrld.md":	{Data: []byte("Title: Post 1")},
+      "hey_world2.md":	{Data: []byte("Title: Post 2")},
+    }
 
-  posts := blogger.NewPostsFromFS(fs)
+    posts, err := blogger.NewPostsFromFS(fs)
 
-  if len(posts) != len(fs) {
-    t.Errorf("got %d posts but wanted %d", len(posts), len(fs))
-  }
+    if err != nil {
+      t.Fatal(err)
+    }
+
+    if len(posts) != len(fs) {
+      t.Errorf("got %d posts but wanted %d", len(posts), len(fs))
+    }
+
+    got := posts[0]
+    want := blogger.Post{Title: "Post 1"}
+
+    if !reflect.DeepEqual(got, want) {
+      t.Errorf("got %+v, want %+v", got, want)
+    }
+  })
+
+  t.Run("returns error when can't open file", func(t *testing.T) {
+    _, err := blogger.NewPostsFromFS(StubFailingFS{})
+
+    if err == nil {
+      t.Errorf("expected an error")
+    }
+  })
+}
+
+
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+  return nil, errors.New("uh oh")
 }
